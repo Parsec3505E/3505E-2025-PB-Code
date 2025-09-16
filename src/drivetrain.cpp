75d9cff2f5d37cd96162052a00a788e72f4bc203
devv686 <158228923+devv686@users.noreply.github.com>
2025-09-16 19:30:05 -0400

    Update drivetrain.cpp

--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -54,7 +54,7 @@ float Drivetrain::angleDiff(float angle1, float angle2)
     return angle2 - angle1;
 }
 // this function resets encoders
-void Drivetrain::resetMotor()
+void Drivetrain::resetMotors()
 {
     leftSide->tare_position();
     rightSide->tare_position();
