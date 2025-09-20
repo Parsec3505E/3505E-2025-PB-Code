965fdfd93e7fcc6c843e91ff574cfcd2704f2e5b
devv686 <158228923+devv686@users.noreply.github.com>
2025-09-20 11:21:52 -0400

    Update intake.hpp

--- include/intake.hpp
+++ include/intake.hpp
@@ -1,1 +1,27 @@
+#ifndef INTAKE_HPP
+#define INTAKE_HPP
 
+#include "api.h"
+
+class Intake
+{
+    public:
+        // Variables
+        bool controllerReversed = false;
+        bool isRunning = false;
+        
+        // Intake Setup
+        Intake(int intakePort;
+        ~Intake();
+
+        // Functions for driver control
+        void updateIntake(pros::Controller &master);
+
+        // Miscellaneous functions
+        void setSpeed(float speed);
+
+        // Internal classes (i.e. motor groups, sensors)
+        pros::Motor* intakeMotor;
+};
+
+#endif
